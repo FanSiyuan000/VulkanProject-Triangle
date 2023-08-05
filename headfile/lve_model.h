3f107e628b68406183e09b453be7cad8c199d94d
Siyuan Fan <1320577464@qq.com>
2023-08-05 14:34:22 +0800

    Index & Staging Buffer

--- headfile/lve_model.h
+++ headfile/lve_model.h
@@ -19,7 +19,12 @@ namespace lve {
             static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
         };
 
-        LveModel(LveDevice& device, const std::vector<Vertex>& vertices);
+        struct Builder{
+            std::vector<Vertex> vertices{};
+            std::vector<uint32_t> indices{};
+        };
+
+        LveModel(LveDevice& device, const LveModel::Builder &builder);
         ~LveModel();//见first_app.h 18行注释
         //模型类管理vulkan缓冲区和内存对象
 
@@ -31,11 +36,18 @@ namespace lve {
 
     private:
         void createVertexBuffers(const std::vector<Vertex>& vertices);
+        void createIndexBuffers(const std::vector<uint32_t>& indices);
 
         LveDevice& lveDevice;
+
         VkBuffer vertexBuffer;
         VkDeviceMemory vertexBufferMemory;
         //对于vulkan的缓冲区对象，缓冲区及其分配的内存是两个单独的对象，而不是自动为缓冲区分配内存
         uint32_t vertexCount;
+
+        bool hasIndexBuffer = false;
+        VkBuffer indexBuffer;
+        VkDeviceMemory indexBufferMemory;
+        uint32_t indexCount;
     };
 }
